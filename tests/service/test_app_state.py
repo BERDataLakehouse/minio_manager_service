97e63b252424e0b8f546e2be8d5eaebcb4a33ff3
Tianhao-Gu <tgu@anl.gov>
2026-03-14 19:26:12 -0500

    run formatter

--- tests/service/test_app_state.py
+++ tests/service/test_app_state.py
@@ -6,7 +6,6 @@ from unittest.mock import AsyncMock, MagicMock, patch
 import pytest
 from fastapi import FastAPI
 
-from src.service import app_state
 from src.service.app_state import (
     AppState,
     RequestState,
